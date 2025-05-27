6203cf0828645f0966984bfd699e5415fb6a0c9c
NiallJoeMaher <nialljoemaher@gmail.com>
2025-05-27 08:44:20 +0100

    Refactor Rollup configuration to import package.json as a module for improved compatibility

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-const packageJson = require("./package.json");
+import packageJson from "./package.json" with { type: "json" };
 
 export default [
   {
